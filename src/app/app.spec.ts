be0d993b823622f8741fe6514783873e1f094515
vcircosta <valentincircosta@gmail.com>
2025-09-04 10:56:50 +0200

    Mise a jour de routes

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app';
 import { RouterOutlet } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterOutlet],
+      imports: [AppComponent, RouterTestingModule], // <-- remplace RouterOutlet par RouterTestingModule
     }).compileComponents();
   });
 
