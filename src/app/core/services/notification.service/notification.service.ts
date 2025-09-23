ada4fc0baa863f23f0deecae9ad5bc63c25d7756
vcircosta <valentincircosta@gmail.com>
2025-09-23 09:06:59 +0200

    Linting du code

--- src/app/core/services/notification.service/notification.service.ts
+++ src/app/core/services/notification.service/notification.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationService {
-    success(msg: string) { console.warn('[SUCCESS]', msg); }
+    success(msg: string) { console.log('[SUCCESS]', msg); }
     error(msg: string) { console.error('[ERROR]', msg); }
-    info(msg: string) { console.warn('[INFO]', msg); }
+    info(msg: string) { console.info('[INFO]', msg); }
 }
